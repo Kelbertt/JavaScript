aea7a42c78118dd9743e57bb4fa2a25789a9eae6
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-20 20:38:23 -0300

    create index.html | index.js

---
+++ Challenge_Full_Stack_8_week/2_Extern_JS_Exercise/index.js
@@ -0,0 +1,13 @@
+let olderPersonName = prompt("Digite o nome da pessoa mais velha")
+let olderPersonAge = prompt("igite a idade da pessoa mais velha")
+
+let youngerPersonName = prompt("Digite o nome da pessoa mais nova ")
+let youngerPersonAge = prompt("Digite a idade da pessoa mais nova")
+
+let ageDifference = olderPersonAge -  youngerPersonAge 
+
+alert(
+    "Pessoa mais velha:  "+ olderPersonName +" \n Idade: " + olderPersonAge +
+    "\n\n Pessoa mais nova: "+ youngerPersonName + "\n Idade: " + youngerPersonAge +
+    "\n\nDiferença de idade: " + ageDifference
+)
