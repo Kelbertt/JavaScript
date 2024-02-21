8cf698e2c58105e68abe4d45a72ec94b7e35103b
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-21 19:56:01 -0300

    Update index.js
    update and finish index.js usind the math. library anda moment library of js

--- JS_Full_Stack_8/5_Mini_App/index.js
+++ JS_Full_Stack_8/5_Mini_App/index.js
@@ -6,3 +6,20 @@ let today = moment()
 
 let datediff = today - departureDate
 
+let chosenoption = prompt("Escolha como gostaria de exibir o tempo de partida \n 1- Segundos \n 2- Minutos \n 3- Horas \n 4- Dias")
+
+if (chosenoption == "1") {
+    let secondsofdeparture = Math.round(datediff / 1000)
+    alert("Tempo de vôo: " + secondsofdeparture + " segundos")
+} else if(chosenoption == "2"){
+    let minutesofdeparture = Math.round(datediff / 1000 / 60)
+    alert("Tempo de vôo: " + minutesofdeparture + " minutos")
+}else if(chosenoption == "3"){
+    let hoursofdeparture = Math.round(datediff / 1000 / 3600)
+    alert("Tempo de vôo: " + hoursofdeparture + " horas")
+}else if(chosenoption == "4"){
+    let daysofdeparture = Math.round(datediff / 1000 / 3600 / 24)
+    alert("Tempo de vôo: " + daysofdeparture + " dias")
+}else {
+    alert("Opção inválida")
+}
