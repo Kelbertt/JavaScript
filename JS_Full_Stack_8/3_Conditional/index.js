a5b8331c0b5d54895249a616196eb6c358641841
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-21 14:22:48 -0300

    create Conditional folder AND create index.js

---
+++ JS_Full_Stack_8/3_Conditional/index.js
@@ -0,0 +1,11 @@
+let velocity = 10
+
+if(velocity < 40) {
+  console.log("Velocidade baixa!")
+  } else if(velocity <= 100){
+  console.log("Cuidado! Próximo a velocidade de risco")
+  } else {
+  console.log("Velocidade de Risco!")
+  }
+
+(velocity > 100) ? console.log("Velocidade maior que 100") : console.log("Velocidade menor que 100")
