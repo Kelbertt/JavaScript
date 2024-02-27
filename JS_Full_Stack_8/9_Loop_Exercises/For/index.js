97cd8bf0495d1ed6f0f6b8185fcc161796597b0f
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-27 00:13:27 -0300

    Update index.js
    update and finish index.js

--- JS_Full_Stack_8/9_Loop_Exercises/For/index.js
+++ JS_Full_Stack_8/9_Loop_Exercises/For/index.js
@@ -4,4 +4,22 @@ let charToReplace = prompt("Qual caractere vocÊ deseja substituir?")
 
 let replacementChar = prompt("Por qual caracter você deseja substituir?")
 
-console.log(spaceship, charToReplace, replacementChar)
+let newSpaceship = ""  // utilizado para verificar se o caracter atual é o que eu quero substituir
+
+for (let pos = 0; pos < spaceship.length; pos++) {
+    if(spaceship[pos] == charToReplace ) {
+        newSpaceship += replacementChar
+    } else{
+        newSpaceship += spaceship[pos]
+    }
+}
+
+alert("O novo nome da nave é " + newSpaceship )
+
+// Elemental
+// e
+// h
+
+// pos = 0   E  e   newSpaceship = E
+// pos = 0   l  e   newSpaceship = l
+// pos = 0   e  e   newSpaceship = h
