362ad98152c89091472b9e1787e08d5206762762
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-02-27 00:04:29 -0300

    update index.html
    update index.html with css style

---
+++ JS_Full_Stack_8/9_Loop_Exercises/For/index.js
@@ -0,0 +1,7 @@
+let spaceship = prompt("Digite o nome da nave")
+
+let charToReplace = prompt("Qual caractere vocÊ deseja substituir?")
+
+let replacementChar = prompt("Por qual caracter você deseja substituir?")
+
+console.log(spaceship, charToReplace, replacementChar)
