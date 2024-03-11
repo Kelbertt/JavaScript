30af9e9e46eb5faea66eee74a7806d0b00bf86be
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-10 23:34:09 -0300

    Update index.js

--- JS_Full_Stack_8/10_Function/index.js
+++ JS_Full_Stack_8/10_Function/index.js
@@ -1,1 +1,20 @@
+let spaceshipName = prompt("Digite o nome da nave:")
 
+let spaceshipVelocity = 0
+
+let chosenOption
+
+function showMenu() {
+    let option
+    while(option != "1" &&  option != "2" &&  option != "3" &&  option != "4") {
+        option = prompt("O que deseja fazer?\n" +
+        "1- Acelera a nave em 5km/s\n" +
+        "2- Desacelerar a nave em 5km/s\n" +
+        "3- Imprimir dados de bordo\n" +
+        "4- Sair do programa")
+    }
+
+    return option
+}
+
+console.log(showMenu)
