4cf5d65ce1f2861485603e193f97a7612cf9206f
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-10 23:39:39 -0300

    Update index.js
    add do while to chose option

--- JS_Full_Stack_8/10_Function/index.js
+++ JS_Full_Stack_8/10_Function/index.js
@@ -8,7 +8,7 @@ function showMenu() {
     let option
     while(option != "1" &&  option != "2" &&  option != "3" &&  option != "4") {
         option = prompt("O que deseja fazer?\n" +
-        "1- Acelera a nave em 5km/s\n" +
+        "1- Acelerar a nave em 5km/s\n" +
         "2- Desacelerar a nave em 5km/s\n" +
         "3- Imprimir dados de bordo\n" +
         "4- Sair do programa")
@@ -17,4 +17,6 @@ function showMenu() {
     return option
 }
 
-console.log(showMenu())
+do {
+    chosenOption = showMenu()
+} while(chosenOption != "4")
