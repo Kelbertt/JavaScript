a1da3cfdb4e2d69e0a5db76d2203ffc8be3902fd
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-11 23:58:25 -0300

    Update index.js

--- JS_Full_Stack_8/11_Function_Exercises/index.js
+++ JS_Full_Stack_8/11_Function_Exercises/index.js
@@ -1,6 +1,19 @@
 let velocity = "150 km/s"
 
 function slowDown(velocity) {
-    
+    let desaceleration = 20
+
+    while(velocity > 0) {
+        printer(velocity)
+        velocity -= desaceleracion
+    }
+
+    alert("Nave parada. As comportas podem ser abertas.")
 
 }
+
+let spaceshipVelocity = 150
+
+slowDown(spaceshipVelcity, function(velocity){
+    console.log("Velocidade Atual:  " + velocity)
+})
