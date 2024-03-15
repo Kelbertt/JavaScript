f6615a4a13ae550cbd05a5bc189fa7617f9e85e4
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-15 16:40:22 -0300

    Update index.js

--- JS_Full_Stack_8/13_Objects/Introduction_V2/index.js
+++ JS_Full_Stack_8/13_Objects/Introduction_V2/index.js
@@ -10,4 +10,12 @@ let spaceship = {
   
   
   
+  let spaceships = [
+    {name: "Elemental", crewQuantity: 10 },
+    {name: "Colossus", crewQuantity: 8 },
+    {name: "Helmete", crewQuantity: 15 }
+  ]
   
+  spaceships.forEach(spaceship => {
+    alert(spaceship.name + " tem " + spaceship.crewQuantity + " tripulantes.")
+  })  
