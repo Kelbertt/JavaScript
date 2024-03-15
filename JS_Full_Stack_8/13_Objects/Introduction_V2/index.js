d8fb55e82e074afbde4d9b6ab650c0c93227d5e3
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-15 16:38:46 -0300

    Update index.js

---
+++ JS_Full_Stack_8/13_Objects/Introduction_V2/index.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    name: "Supernova",
+    type: "Batalha",
+    crew: ["Cap. Silva", "Ana Júlia", "Thiago"]
+  }
+  
+  spaceship.crew.push("Ten. Fernanda")
+  
+  console.log(spaceship)
+  
+  
+  
+  
