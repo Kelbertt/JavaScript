682d7bb6ab259a92071e40483a7a8fa142d44d69
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-21 00:13:39 -0300

    Update index.js

--- JS_Full_Stack_8/14_Classes/index.js
+++ JS_Full_Stack_8/14_Classes/index.js
@@ -1,7 +1,7 @@
 class Spaceship {
-    constructor(name,crewQuantity) {
+    constructor(name, crewQuantity) {
         this.name = name
-        this.crewQuantity= crewQuantity
+        this.crewQuantity = crewQuantity
         this.isHitched = false
         this.entraceDoorsOpen = false
     }
@@ -15,10 +15,10 @@ class Spaceship {
 function showMenu() {
     let chosenOption
     while(chosenOption != "1" && chosenOption != "2" && chosenOption != "3"){
-        chosenOption = prompt ("O que deseja fazer?\n" +
-        "1- Engatar nave\n" +
-        "2- Imprimir naves\n" +
-        "3- Sair do programa")
+        chosenOption = prompt ( "O que deseja fazer?\n" +
+                                "1- Engatar nave\n" +
+                                "2- Imprimir naves\n" +
+                                "3- Sair do programa")
     }
     return chosenOption
 }
