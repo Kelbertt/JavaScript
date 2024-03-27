be6aaadeab110edcdac043be309bb861e0b03ae4
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-26 22:12:10 -0300

    Update selecting-elements.js
    use document.querySelectorAll

--- JS_Full_Stack_8/19_DOM/Elements_Dom/selecting-elements.js
+++ JS_Full_Stack_8/19_DOM/Elements_Dom/selecting-elements.js
@@ -1,4 +1,4 @@
 function show() {
-    let elements = document.getElementsByName("phone")
-    console.log(elements[0].value)
+    let elements = document.querySelectorAll("div#phones input[name='phone']")
+    console.log(elements)
 }
