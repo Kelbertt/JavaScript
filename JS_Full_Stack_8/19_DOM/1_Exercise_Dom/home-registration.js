84ff6ea1fbee5ff071b206f136e456f60479eeaa
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-03-27 02:45:15 -0300

    updare home-registration.js

--- JS_Full_Stack_8/19_DOM/1_Exercise_Dom/home-registration.js
+++ JS_Full_Stack_8/19_DOM/1_Exercise_Dom/home-registration.js
@@ -1,11 +1,25 @@
-function add() {
-
-}
-
-function remove() {
-    
-}
 
 function saveHouse() {
+    let area = document.querySelector("input[id='area']").value
+    let number = document.querySelector("input[id='number']").value
+    let neighborhood = document.querySelector("input[id='neighborhood']").value
+    let city = document.querySelector("input[id='city']").value
+
+    let newListValue = document.createElement("li")
+    newListValue.innerText = area + "m², número " + number + " (" +  neighborhood + " - " + city + ")"
+    
+    let removeButton = document.createElement("button")
+    removeButton.type = "button"
+    removeButton.innerText = "Remover"
+    removeButton.setAttribute("onclick", "removeHouse(this)")
+
+    newListValue.appendChild(removeButton)
+
+    document.getElementById("house-list").appendChild(newListValue)
+}
+
+function removeHouse(button) {
+    let liToRemove = button.parentNode
+    document.getElementById("house-list").removeChild(liToRemove)
     
 }
