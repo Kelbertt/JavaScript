cc4d860a4145dfa0c5fb0f9387f6ae9495b32fb0
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-04-28 03:31:44 -0300

    Update forms_event.js

--- JS_Full_Stack_8/18_Events/2_Forms_Event/forms_event.js
+++ JS_Full_Stack_8/18_Events/2_Forms_Event/forms_event.js
@@ -1,4 +1,4 @@
 function save() {
-    let name = document.querySelector("input[name=name]").nodeValue
+    let name = document.querySelector("input[name='name']").Value
     let programming = document.querySelector("select[name='programming-lang']").value
 }
