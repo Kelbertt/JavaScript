25fefd4dcfb5ab2912c23ffe88f839c0c33f2b20
Kaio Monte Cristo <113322785+Kelbertt@users.noreply.github.com>
2024-04-28 04:17:53 -0300

    update files

---
+++ JS_Full_Stack_8/19_DOM/Manipulating_Attr/manipulating-attr.js
@@ -0,0 +1,10 @@
+function verifyEnablement() {
+    let selectValue = document.querySelector("select").value
+    if(selectValue == "disabled") {
+        document.querySelector("input[name='some-text']").disabled = true
+        let pElement = document.getElementById("message")
+        pElement.innerText = "Desabilitado"
+        pElement.style.color = "white"
+        pElement.style.backgroundColor = "red"
+    }
+}
