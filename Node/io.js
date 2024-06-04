25cb326ea98d11f929285fceeb38a02fd514e05c
Kaio Monte Cristo <20201100013@pq.uenf.br>
2024-06-04 20:04:31 -0300

    Update io.js
    using process

--- Node/io.js
+++ Node/io.js
@@ -1,3 +1,8 @@
 const process = require('process');
 
-console.log(process.argv);
+console.log('digite seu nome: ')
+
+process.stdin.on('data', (keyboard) => {
+    process.stdout.write(`Texto do usuário: ${keyboard}`);
+    process.exit();    
+})
