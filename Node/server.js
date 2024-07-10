9cf1c30f2fdb5093eb2a6135b11692e5b6c8e058
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2024-07-09 23:12:50 -0300

    Update server.js
    Insert a "resposta" to the create server using method res.end('<h1>Hello World<h1>')

--- Node/server.js
+++ Node/server.js
@@ -4,6 +4,7 @@ const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
     res.statusCode = 200;
+    res.end('<h1>Hello World<h1>');
 });
 
 server.listen(3000, () => {
