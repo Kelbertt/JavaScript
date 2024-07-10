5efc28fe931216a9a96201b3dd08a126d57409c4
Kaio Monte Cristo <kaiomontecristo15@gmail.com>
2024-07-09 23:03:06 -0300

    Update server.js

--- Node/server.js
+++ Node/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
+    res.statusCode = 200;
 });
 
 server.listen(3000, () => {
